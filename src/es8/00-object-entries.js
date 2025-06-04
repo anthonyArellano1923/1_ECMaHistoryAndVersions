02cce042a7acda467ff2b1c108c014c2b519a518
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-06-04 15:49:28 -0400

    Class 12: object entries and values.

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,8 @@
+const countries = {
+  MX: 'Mexico',
+  PR: 'Portugal',
+  CO: 'Colombia',
+  VZ: 'Venezuela',
+}
+
+console.log(Object.entries(countries))
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,9 @@
+const countries = {
+  MX: 'Mexico',
+  PR: 'Portugal',
+  CO: 'Colombia',
+  VZ: 'Venezuela',
+}
+
+console.log(Object.values(countries))
+console.log(Object.keys(countries))
