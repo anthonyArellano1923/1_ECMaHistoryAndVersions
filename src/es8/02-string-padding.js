c9a6705bfe436369e632f91ffc8e91afe15f0983
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-06-04 16:10:37 -0400

    Class 13: strings padding & trailing commas.

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,4 @@
+const string1 = 'Hello'
+
+console.log(string1.padStart(15,'_'))
+console.log(string1.padEnd(15,'_'))
---
+++ src/es8/03-trailing-commas.js
@@ -0,0 +1,5 @@
+const list1 = [0, 1, 2, 3, 4, 5, , , , ]
+const list2 = [0, , , , 1, 2, , , , 3, 4, 5, , ]
+
+console.log(list1)
+console.log(list2)
