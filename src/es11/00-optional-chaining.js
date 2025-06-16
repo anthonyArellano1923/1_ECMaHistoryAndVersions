04c1057e28b2a6eb970c7d932be3fd100d3f83ce
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-06-16 10:52:55 -0400

    Class 19: optional chaining.

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,14 @@
+const data = {
+  Ana: {
+    country: 'MX'
+  },
+  Luis: {
+    country: 'CO'
+  }
+}
+
+console.log(data.Ana?.country)
+console.log(data.Tadeo?.age)
+console.log(data.Luis?.height)
+
+
