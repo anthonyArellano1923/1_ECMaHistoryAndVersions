39ab4a62262c844fede42d9ca5144531893653f0
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-06-16 11:05:28 -0400

    Class 20: big int, nullish.

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const bigNumber = 2367623645645875635346n
+const anotherBigNumber = BigInt(92345645656793456535578)
+
+console.log(bigNumber, anotherBigNumber)
---
+++ src/es11/02-nullish.js
@@ -0,0 +1,4 @@
+const number = null
+const isNumber = number ?? 5
+
+console.log(isNumber)
