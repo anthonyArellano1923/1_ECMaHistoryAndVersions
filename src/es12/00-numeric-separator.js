01465c9a2da30221ebb49548c7d86197acd1c65e
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-06-16 21:28:27 -0400

    Class 23: nueric separator & replace all.

---
+++ src/es12/00-numeric-separator.js
@@ -0,0 +1,6 @@
+const number1 = 1_000
+const number2 = 1_000_000
+const number3 = 1_000_000_000
+const number4 = 1_000_000_000_000
+
+console.log(number1, number2, number3, number4)
---
+++ src/es12/01-replace-all.js
@@ -0,0 +1,4 @@
+const phrase1 = 'I love every pizza in the planet!'
+const phrase2 = phrase1.replaceAll('I love', 'They hate')
+
+console.log(`${phrase1} \n${phrase2}`)
